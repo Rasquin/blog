6594070ebefc7ccc6d9f2b92a7dc6e537d306679
Ubuntu <ubuntu@ip-172-31-93-164.ec2.internal>
2019-09-13 17:53:26 +0000

    models and forms added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """
+    A single Blog post
+    """
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    # auto_now_add = True, which means that as soon as the record is created, then the current date and time will be added into that field.
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
